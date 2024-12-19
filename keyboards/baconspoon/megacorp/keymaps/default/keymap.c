db2876a11f098df5e071c479402826feeb16603f
baconspoon85 <jondgatson@outlook.com>
2024-12-19 01:21:32 -0500

    fix megacorp keymaps

--- keyboards/baconspoon/megacorp/keymaps/default/keymap.c
+++ keyboards/baconspoon/megacorp/keymaps/default/keymap.c
@@ -5,29 +5,31 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
 	       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,           KC_Y,          KC_U,    KC_I,    KC_O,    KC_P,
-	       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,          KC_J,    KC_K,    XXXXXXX, KC_L,
-	       KC_Z,    XXXXXXX, KC_X,    KC_C,    KC_V,           KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,
-	       KC_LCTL, KC_LGUI, KC_LALT, XXXXXXX, LT(1, KC_BSPC), LT(2, KC_SPC), XXXXXXX, KC_DEL,  KC_ESC,  KC_ENT
+	       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,           KC_H,          KC_J,    KC_K,             KC_L,
+	       KC_Z,             KC_X,    KC_C,    KC_V,           KC_B,          KC_N,    KC_M,    KC_COMM, KC_DOT,
+	       KC_LCTL, KC_LGUI, KC_LALT,          LT(1, KC_BSPC), LT(2, KC_SPC),          KC_DEL,  KC_ESC,  KC_ENT
 	),
 
   [1] = LAYOUT(
 	       KC_1,    KC_2,    KC_3,    KC_4,    KC_5,           KC_6,          KC_7,    KC_8,    KC_9,    KC_0,
-	       KC_BSLS, KC_MINS, KC_EQL,  KC_F,    KC_G,           KC_RGHT,       KC_LEFT, KC_DOWN, XXXXXXX, KC_UP,
-	       KC_TRNS, XXXXXXX, KC_LBRC, KC_RBRC, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_QUOT, KC_SCLN, KC_SLSH,
-	       KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, KC_TRNS,        KC_TRNS,       XXXXXXX, KC_TRNS, KC_TRNS, KC_TRNS
+	       KC_BSLS, KC_MINS, KC_EQL,  KC_F,    KC_G,           KC_RGHT,       KC_LEFT, KC_DOWN,          KC_UP,
+	       KC_TRNS,          KC_LBRC, KC_RBRC, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_QUOT, KC_SCLN, KC_SLSH,
+	       KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,        KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS
 	),
+
     
   [2] = LAYOUT(
 	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS,          KC_TRNS,
+	       KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	       KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,        KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS
 	),
+
     
   [3] = LAYOUT(
 	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+	       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS,          KC_TRNS,
+	       KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS,        KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+	       KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,        KC_TRNS,                KC_TRNS, KC_TRNS, KC_TRNS
 	),
 };
